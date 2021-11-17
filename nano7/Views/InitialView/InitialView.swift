47e3f594d4d7fd9e48f65eab0741754c8255f79f
Layla Emanuele <69046321+LaylaEmanuele@users.noreply.github.com>
2021-11-17 20:47:07 -0300

    feat: create all the elements of the screen and set constraints

--- nano7/Views/InitialView/InitialView.swift
+++ nano7/Views/InitialView/InitialView.swift
@@ -20,52 +20,74 @@ class InitialView: UIView {
     }
     
     //MARK: - Views
-    lazy var titleLabel = makeTitleLabel()
+    lazy var titleLabel = makeLabel(text: "Let's Walk and Draw")
+    lazy var bottomLabel = makeLabel(text: "Let's Go")
     lazy public var button = makeButton()
+    lazy var mainImage = makeImage(imageName: "mainImage")
+    
     
     //MARK: - Layout Setup
     
 }
 // MARK: - Layout Setup
 extension InitialView {
-    private func makeTitleLabel() -> UILabel {
+    private func makeImage(imageName: String) -> UIImageView {
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
+    private func makeLabel(text: String) -> UILabel {
         let label = UILabel()
-        label.text = "Let's walk and Draw"
+        label.text = text
+        label.font = UIFont(name: "Combo", size: 36.0)
         return label
     }
 
     private func makeButton() -> UIButton {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 55))
-        button.backgroundColor = .systemCyan
-        button.setTitleColor(.white, for: .normal)
-        button.setTitle("Tap Me!", for: .normal)
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "playButton"), for: .normal)
         return button
     }
     
     private func addViews() {
         addSubview(titleLabel)
+        addSubview(bottomLabel)
         addSubview(button)
+        addSubview(mainImage)
     }
     
     //MARK: - Add Constraints
     private func addConstraints() {
         titleAutoLayout()
+        mainImageAutoLayout()
         buttonAutoLayout()
-    }
-    func buttonAutoLayout() {
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.trailingAnchor.constraint(equalTo: self.leadingAnchor, constant: 80).isActive = true
-        button.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor, constant: -300).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 75).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 87).isActive = true
-    }
-    func titleAutoLayout() {
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.trailingAnchor.constraint(equalTo: self.leadingAnchor, constant: 80).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor, constant: -300).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 75).isActive = true
-        titleLabel.widthAnchor.constraint(equalToConstant: 87).isActive = true
+        bottomLabelAutoLayout()
     }
     
+    func titleAutoLayout() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: mainImage.topAnchor).isActive = true
+    }
+    
+    func mainImageAutoLayout() {
+        mainImage.translatesAutoresizingMaskIntoConstraints = false
+        mainImage.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        mainImage.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor, constant: -100).isActive = true
+        mainImage.layer.zPosition = -1
+    }
+    
+    func buttonAutoLayout() {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor, constant: 10).isActive = true
+        button.centerYAnchor.constraint(equalTo: mainImage.bottomAnchor, constant: 100).isActive = true
+    }
+    
+    func bottomLabelAutoLayout() {
+        bottomLabel.translatesAutoresizingMaskIntoConstraints = false
+        bottomLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        bottomLabel.centerYAnchor.constraint(equalTo: button.bottomAnchor, constant: 40).isActive = true
+    }
     
 }
