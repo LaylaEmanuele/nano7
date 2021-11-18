82c3c726db02789c242938b3e743d1a510e500db
Rafael Basso <44483784+RafaelBassoDev@users.noreply.github.com>
2021-11-18 18:44:52 -0300

    Created ShareViewTabBar controller, wrapper, delegate and XIB

---
+++ nano7/XIBs/ShareView/ShareViewTabBarDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  ShareViewTabBarDelegate.swift
+//  nano7
+//
+//  Created by Rafael Basso on 18/11/21.
+//
+
+import UIKit
+
+protocol ShareViewTabBarDelegate {
+    
+    func backButtonPresesd()
+    
+    func downloadButtonPressed()
+    
+    func shareButtonPressed()
+    
+}
