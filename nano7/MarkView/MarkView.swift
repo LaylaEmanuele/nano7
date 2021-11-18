3325eb3031fb0253466ff80cbf7de803c0acc48c
PhilippeVenancio <87039791+PhilippeVenancio@users.noreply.github.com>
2021-11-18 13:11:27 -0300

    click to generate pins
    feat:  now when the user click, the pins with title, appear in screen

--- nano7/MarkView/MarkView.swift
+++ nano7/MarkView/MarkView.swift
@@ -6,27 +6,91 @@
 //
 
 import UIKit
+import CoreLocation
 import MapKit
 
-class MarkViewController: UIViewController {
+class MarkViewController: UIViewController, CLLocationManagerDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
+    var numberOfPins = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addPinMap(coordinate: CLLocationCoordinate2D(latitude: -15.84173355916682, longitude: -48.04400844933156))
-        addPinMap(coordinate: CLLocationCoordinate2D(latitude: -15.83373814676899, longitude: -47.95538323261778))
+        let gestureRecognizer = UITapGestureRecognizer(target: self,
+                                                       action:#selector(handleTap))
+            gestureRecognizer.delegate = self
+            mapView.addGestureRecognizer(gestureRecognizer)
         
-        //let region = MKCoordinateRegion()
+//        addPinMap(CLLocationCoordinate2D(latitude: -15.84173355916682, longitude: -48.04400844933156))
+//        addPinMap(CLLocationCoordinate2D(latitude: -15.83373814676899, longitude: -47.95538323261778))
         
     }
     
-    func addPinMap(coordinate: CLLocationCoordinate2D){
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinate
-        mapView.addAnnotation(annotation)
+    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
+    
+    @objc func handleTap(gestureRecognizer: UITapGestureRecognizer) {
+        
+        let location = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        // Add annotation:
+        addPinMap(coordinate)
+    
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+//        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        if let location = locations.first{
+            
+            manager.stopUpdatingLocation()
+            render(location)
+            
+//            print("locations = \(location.coordinate.latitude) \(location.coordinate.longitude)")
+        }
+        
+    }
+    
+    func render(_ location: CLLocation){
+        
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.009,
+                                    longitudeDelta: 0.009)
+        
+        let region = MKCoordinateRegion(center: coordinate,
+                                        span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        addPinMap(coordinate)
+        // TELL ME WHY-> Para confirmar a numeração correta dos pins
+        numberOfPins = 1
+    }
+    
+    func addPinMap(_ coordinate: CLLocationCoordinate2D){
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        numberOfPins = numberOfPins + 1
+        pin.title = "\(numberOfPins)"
+        mapView.addAnnotation(pin)
+    }
+    
+    
     
 }
