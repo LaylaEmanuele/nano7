fe2936424e3dde0c438756e147915d060dfef586
PhilippeVenancio <87039791+PhilippeVenancio@users.noreply.github.com>
2021-11-18 15:16:55 -0300

    integration with two maps
    Feat: Make the integration

--- nano7/Views/MapView/MapViewController.swift
+++ nano7/Views/MapView/MapViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, Coordinating, CLLocationManagerDelegate {
+class MapViewController: UIViewController, Coordinating, CLLocationManagerDelegate, UIGestureRecognizerDelegate {
     var coordinater: Coordinator?
     var locationManager: CLLocationManager?
     
@@ -23,8 +24,15 @@ class MapViewController: UIViewController, Coordinating, CLLocationManagerDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dealerSelectionView.backgroundColor = .red
         askLocationPerms()
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self,
+                                                       action:#selector(handleTap))
+            gestureRecognizer.delegate = self
+            dealerSelectionView.map.addGestureRecognizer(gestureRecognizer)
+        
+        //addPinMap(CLLocationCoordinate2D(latitude: -15.84173355916682, longitude: -48.04400844933156))
+        
     }
     
     // MARK: - Setup
@@ -36,14 +44,37 @@ class MapViewController: UIViewController, Coordinating, CLLocationManagerDelega
         
     }
     
-    private func askLocationPerms() {
-        locationManager = CLLocationManager()
-        locationManager?.delegate = self
-        locationManager?.requestAlwaysAuthorization()
+    @objc func handleTap(gestureRecognizer: UITapGestureRecognizer) {
+        
+        let location = gestureRecognizer.location(in: dealerSelectionView.map)
+        let coordinate = dealerSelectionView.map.convert(location, toCoordinateFrom: dealerSelectionView.map)
+        
+        // Add annotation:
+        addPinMap(coordinate)
+    
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    
+    private func askLocationPerms() {
+        locationManager = CLLocationManager()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        //locationManager?.requestWhenInUseAuthorization()
+        
+        locationManager?.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus, didUpdateLocations locations: [CLLocation]) {
         if status == .authorizedAlways {
+            
+            if let location = locations.first{
+                
+                manager.stopUpdatingLocation()
+                render(location)
+            }
+            
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
                     print("Poggers")
@@ -54,5 +85,32 @@ class MapViewController: UIViewController, Coordinating, CLLocationManagerDelega
             UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)
         }
     }
+    
+    func render(_ location: CLLocation){
+        
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.009,
+                                    longitudeDelta: 0.009)
+        
+        let region = MKCoordinateRegion(center: coordinate,
+                                        span: span)
+        
+        dealerSelectionView.map.setRegion(region, animated: true)
+        
+        //addPinMap(coordinate)
+        // TELL ME WHY-> Para confirmar a numeração correta dos pins
+        //numberOfPins = 1
+    }
+    
+    func addPinMap(_ coordinate: CLLocationCoordinate2D){
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+//        numberOfPins = numberOfPins + 1
+//        pin.title = "\(numberOfPins)"
+        dealerSelectionView.map.addAnnotation(pin)
+    }
+    
 }
 
